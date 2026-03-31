e2431ee7a043df9765e21f8376be05c0206c7eb4
Maximo Paulos <mpaulosfrancisco@udesa.edu.ar>
2026-03-31 15:29:56 -0300

    Fix router double mounting in frontend entrypoint

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import "./styles/tailwind.css";
@@ -14,9 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <SessionProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <App />
         <ReactQueryDevtools initialIsOpen={false} />
       </SessionProvider>
     </QueryClientProvider>
