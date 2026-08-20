f3141e03056014deb03f86cd7ace0bc407772710
Maximo Paulos <maximopaulos1@gmail.com>
2026-08-20 16:28:00 -0300

    fix(migrations): chain temporary action grants migration onto permission metadata head

--- alembic/versions/0f85dca5b98b_add_temporary_action_grants.py
+++ alembic/versions/0f85dca5b98b_add_temporary_action_grants.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '0f85dca5b98b'
-down_revision: Union[str, None] = '20260820_user_sessions'
+down_revision: Union[str, None] = '20260820_permission_metadata'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
