239b432dfff0c8293665751b68f44bff38d4619c
Maximo Paulos <maximopaulos1@gmail.com>
2026-08-20 17:58:59 -0300

    fix(migrations): chain primary owner migration onto temporary action grants head

--- alembic/versions/20260820_primary_owner.py
+++ alembic/versions/20260820_primary_owner.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 
 revision: str = "20260820_primary_owner"
-down_revision: Union[str, None] = "20260820_permission_metadata"
+down_revision: Union[str, None] = "0f85dca5b98b"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
