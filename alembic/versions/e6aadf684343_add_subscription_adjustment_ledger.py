a4d0289b205212c94d4f484d12321db810d1822a
Maximo Paulos <maximopaulos1@gmail.com>
2026-08-20 19:47:34 -0300

    fix(migrations): chain subscription adjustment ledger onto staff invitation lifecycle head

--- alembic/versions/e6aadf684343_add_subscription_adjustment_ledger.py
+++ alembic/versions/e6aadf684343_add_subscription_adjustment_ledger.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'e6aadf684343'
-down_revision: Union[str, None] = '20260820_guest_search_first_name_index'
+down_revision: Union[str, None] = '8b5d07cc381b'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
