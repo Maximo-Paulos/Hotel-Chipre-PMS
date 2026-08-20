024bad40d8ef138b9613d09d2245780460096ad3
Maximo Paulos <maximopaulos1@gmail.com>
2026-08-20 18:36:58 -0300

    fix(migrations): chain guest search index migration onto primary owner head

--- alembic/versions/20260820_guest_search_first_name_index.py
+++ alembic/versions/20260820_guest_search_first_name_index.py
@@ -11,7 +11,7 @@ from alembic import op
 
 
 revision: str = "20260820_guest_search_first_name_index"
-down_revision: Union[str, None] = "20260820_permission_metadata"
+down_revision: Union[str, None] = "20260820_primary_owner"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
