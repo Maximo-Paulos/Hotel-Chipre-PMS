d3dcbfd0fd72f55b09fbb31c05e05d7f818ab3a3
Maximo Paulos <maximopaulos1@gmail.com>
2026-08-20 19:24:17 -0300

    fix(migrations): chain staff invitation lifecycle migration onto guest search index head

--- alembic/versions/8b5d07cc381b_persist_staff_invitation_lifecycle.py
+++ alembic/versions/8b5d07cc381b_persist_staff_invitation_lifecycle.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '8b5d07cc381b'
-down_revision: Union[str, None] = '20260820_primary_owner'
+down_revision: Union[str, None] = '20260820_guest_search_first_name_index'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
